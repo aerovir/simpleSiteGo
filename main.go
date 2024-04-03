f00a4fae1b312b23809e8541c950c82a416582ac
aerovir <aeroviradmin@gmail.com>
2024-04-04 01:04:19 +0200

    4 lesson

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "method denied", 405)
+		return
+	}
 	w.Write([]byte("Форма создания новой заметки..."))
 }
 
